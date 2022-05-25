92ad3cea2fecc2045cbce37a2d68aa4b0655463f
MalithSena <senanayake.malith@gmail.com>
2022-05-25 18:35:12 +0530

    first commit

---
+++ components/Navbar.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import Link from 'next/link'
+import { AiOutlineShopping } from 'react-icons/ai'
+
+import { useStateContext } from '../context/StateContext'
+
+import {Cart} from './'
+import { useState } from 'react/cjs/react.production.min'
+
+const Navbar = () => {
+  const {showCart, setShowCart, totalQuantities} = useStateContext()
+  
+  return (
+    <div className='navbar-container'>
+      <p>
+        <Link href="/">JSM Headphones</Link>
+      </p>
+
+      <button type="button" className="cart-icon" onClick={()=>{
+        setShowCart(true)
+      }}>
+        <AiOutlineShopping />
+        <span className="cart-item-qty">{totalQuantities}</span>
+      </button>
+
+      {showCart && <Cart />}
+    </div>
+  )
+}
+
+export default Navbar
